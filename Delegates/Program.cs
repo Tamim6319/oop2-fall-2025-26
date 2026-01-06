4211198877a7f4e6a5ba40c630570af98f3be8bd
Tamim6319 <hasantamim6319@gmail.com>
2026-01-06 19:33:55 +0600

    22-49966-3

---
+++ Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class program
+{
+    delegate int operation(int a, int b);
+
+    static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    static int subtract(int x, int y)
+    {
+        return x - y;
+    }
+
+    static int calculate(int a, int b, operation op)
+    {
+        return op(a, b);
+    }
+    static void Main()
+    {
+        int num1 = 9, num2 = 5;
+
+        int addResult = calculate(num1, num2, Add);
+        int subResult = calculate(num1, num2, subtract);
+
+        Console.WriteLine("Addition Result: " + addResult);
+        Console.WriteLine("Subtraction Result : " + subResult);
+
+        Console.ReadKey();
+    }
+}
---
+++ Student Record System/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+interface IStudent
+{
+    void Display();
+}
+
+class Student : IStudent
+{
+    private int[] marks = new int[3];
+    public string Name { get; set; }
+
+    // Indexer
+    public int this[int index]
+    {
+        get { return marks[index]; }
+        set { marks[index] = value; }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Student Name: " + Name);
+        Console.WriteLine("Marks:");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Subject " + (i + 1) + ": " + marks[i]);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+        s.Name = "Kafi";
+
+        s[0] = 80;
+        s[1] = 75;
+        s[2] = 90;
+
+        s.Display();
+    }
+}
